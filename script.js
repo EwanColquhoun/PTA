ccf24c50703672576b9f0c9cad461c338059fecf
Ewan Colquhoun <ewancolquhoun@hotmail.com>
2024-03-19 23:05:36 +0000

    Added color, added js for form reset and submit.

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-console.log("ready!")
 
 // get the new location and replaces href in for backbutton
 let anchors = Array.from(document.getElementsByClassName('cat'))
-// console.log(anchinit)
-// let anchors = anchinit.filter(.hash == '#bid-form')
-console.log(anchors)
 let previous = document.getElementById('previous')
-
 let pre = anchors.forEach(anchor => {
     anchor.addEventListener('click', function(){
-        // console.log(this.hash, 'from funct')
         let prev = this.hash
         previous.setAttribute('href', this.hash)
-        // console.log(previous.getAttribute('href'))
+        console.log(prev)
         return prev
 
     })
 });
 
-// submit to redirect
 
-document.getElementById('submit').addEventListener('onClick', function(){
-    location.replace(pre)
+// submit, feedback then reset form
+let form = document.getElementById('bid-form')
+let subBut = document.getElementById('submit')
+subBut.addEventListener('click', function(){
+    let pName = document.forms["bid-form"]["name"].value
+    alert(`Hi ${pName}, thank you for your bid. Good luck!`)
+    form.reset();
 })
