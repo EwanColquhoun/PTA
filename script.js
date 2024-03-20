a8ef44ed71f685ff6c595fc8abaf0578edcc0f54
Ewan Colquhoun <ewancolquhoun@hotmail.com>
2024-03-20 21:24:56 +0000

    Time now added on checkbox. Submit finish timing

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ btt.addEventListener('click', function(){
     window.scrollTo(0,0);
 });
 
-// get the new location and replaces href in for backbutton
+// get the new location and replaces href in backbutton
 let anchors = Array.from(document.getElementsByClassName('cat'))
 let previous = document.getElementById('previous')
 let pre = anchors.forEach(anchor => {
@@ -33,14 +33,22 @@ bidLink.forEach(bl => {
 let form = document.getElementById('bid-form')
 let subBut = document.getElementById('submit')
 
-// Sets the time for page load re bid.
-window.onload = function(){
-    let timeInput = document.getElementById('timeInput')
+// fills in the bid time when the checkbox is checked
+let box = document.getElementById('consent')
+let timeInput = document.getElementById('timeInput')
+function addTime(){
     let d = new Date()
     let dateGroup = d.toTimeString('en-UK')
     let timeBlock = dateGroup.split(' ')[0].slice(0, 5)
     timeInput.value = timeBlock
 }
+box.addEventListener('click', function(){
+    if (box.checked ==  true){
+        addTime()
+    } else {
+        timeInput.value = "00:00"
+    };
+})
 
 // returns the checkbox state
 function dataConsent(){
@@ -54,6 +62,7 @@ function dataConsent(){
 
 // checks form validation
 function validateForm() {
+
     let x = document.forms["bid-form"]["name"].value;
     let y = document.forms["bid-form"]["email"].value;
     let z = document.forms["bid-form"]["p-search"].value;
@@ -86,3 +95,15 @@ subBut.addEventListener('click', function(){
             }
     } 
 });
+
+// disables submit button after the deadline
+function finishAuction() {
+    subBut.setAttribute('disabled', true);
+    alert("The PTA silent auction has finished. Thank you.")
+    console.log('ended')
+  }
+// not 24hr!
+var timeAtFinish = new Date("5/11/2024 11:59:59 PM").getTime()
+let timeNow = new Date().getTime()
+let offsetMillis = timeAtFinish - timeNow;
+setTimeout(finishAuction, offsetMillis);
