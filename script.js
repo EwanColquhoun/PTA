977ec8b5aa54d2bd5b1a192bb5157f8297980209
Ewan Colquhoun <ewancolquhoun@hotmail.com>
2024-03-20 10:39:15 +0000

    Added js to reset form, added time for gsheets.

--- script.js
+++ script.js
@@ -12,12 +12,24 @@ let pre = anchors.forEach(anchor => {
     })
 });
 
-
-// submit, feedback then reset form
+// Form handlers
 let form = document.getElementById('bid-form')
 let subBut = document.getElementById('submit')
+
+// Sets the time for page load re bid.
+window.onload = function(){
+    console.log('focus')
+    let timeInput = document.getElementById('timeInput')
+    let d = new Date()
+    let dateGroup = d.toTimeString('en-UK')
+    let timeBlock = dateGroup.split(' ')[0].slice(0, 5)
+    timeInput.value = timeBlock
+}
+
+// submit, feedback then reset form
 subBut.addEventListener('click', function(){
     let pName = document.forms["bid-form"]["name"].value
-    alert(`Hi ${pName}, thank you for your bid. Good luck!`)
-    form.reset();
-})
+    alert(`Hi ${pName}, thank you for your bid. Good luck!`);
+    setTimeout(() => {
+        form.reset(), 2000});
+});
