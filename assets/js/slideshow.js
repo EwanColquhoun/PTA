2ddf3785977be0aeba3968ba20b9bc39d41725ff
Ewan Colquhoun <ewancolquhoun@hotmail.com>
2025-01-29 14:09:26 +0000

    Added mobile slideshow on landing page

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,8 +1,12 @@
 // Events slideshow
 let slideIndex = 1;
 let slideIndex2 = 1;
+let mobSlideIndex = 1;
+let mobSlideIndex2 = 1;
 showSlides(slideIndex);
 showSlides2(slideIndex2);
+mobShowSlides(mobSlideIndex)
+mobShowSlides2(mobSlideIndex2)
 
 // Next/previous controls
 function plusSlides(n) {
@@ -13,6 +17,14 @@ function plusSlides2(m) {
   showSlides2(slideIndex2 += m);
 }
 
+function mobPlusSlides(sl) {
+  mobShowSlides(mobSlideIndex += sl);
+}
+
+function mobPlusSlides2(sl2) {
+  mobShowSlides2(mobSlideIndex2 += sl2);
+}
+
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
@@ -20,6 +32,14 @@ function currentSlide(n) {
 
 function currentSlide2(m) {
   showSlides2(slideIndex2 = m);
+}
+
+function mobCurrentSlide(sl) {
+  mobShowSlides(mobSlideIndex = sl);
+}
+
+function mobCurrentSlide2(sl2) {
+  mobShowSlides2(mobSlideIndex2 = sl2);
 }
 
 function showSlides(n) {
@@ -52,4 +72,36 @@ function showSlides2(m) {
   }
   slides2[slideIndex2-1].style.display = "flex";
   dots2[slideIndex2-1].className += " active";
+}
+
+function mobShowSlides(sl){
+  let i;
+  let slides = document.getElementsByClassName("small-fund-card");
+  let dots = document.getElementsByClassName("mob-dot");
+  if (sl > slides.length) {mobSlideIndex = 1}
+  if (sl < 1) {mobSlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[mobSlideIndex-1].style.display = "flex";
+  dots[mobSlideIndex-1].className += " active";
+}
+
+function mobShowSlides2(sl2){
+  let i;
+  let slides = document.getElementsByClassName("small-soc-card");
+  let dots = document.getElementsByClassName("mob-dot2");
+  if (sl2 > slides.length) {mobSlideIndex2 = 1}
+  if (sl2 < 1) {mobSlideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[mobSlideIndex2-1].style.display = "flex";
+  dots[mobSlideIndex2-1].className += " active";
 }
